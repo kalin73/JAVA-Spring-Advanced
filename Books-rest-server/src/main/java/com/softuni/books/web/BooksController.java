b1a28056a4519414ed3310dddf3abd7e7764b1b9
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-04-29 12:37:21 +0300

    Added "CrossOrigin" annotations.

--- Books-rest-server/src/main/java/com/softuni/books/web/BooksController.java
+++ Books-rest-server/src/main/java/com/softuni/books/web/BooksController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,17 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.softuni.books.model.dto.BookDTO;
+import com.softuni.books.repository.BookRepository;
 import com.softuni.books.service.BookService;
 
 @RestController
 @RequestMapping("/api/books")
 public class BooksController {
 	private final BookService bookService;
+	private final BookRepository bookRepository;
 
-	public BooksController(BookService bookService) {
+	public BooksController(BookService bookService, BookRepository bookRepository) {
 		this.bookService = bookService;
+		this.bookRepository = bookRepository;
 	}
 
+	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<BookDTO>> getAllBooks() {
 		return ResponseEntity.ok(bookService.getAllBooks());
@@ -38,19 +43,16 @@ public class BooksController {
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<BookDTO> deleteBookById(@PathVariable Long id) {
-		this.bookService.deleteById(id);
-
-		return ResponseEntity.noContent().build();
+	@CrossOrigin
+	public void deleteBookById(@PathVariable Long id) {
+		this.bookRepository.findById(id).ifPresent(bookRepository::delete);
 	}
 
 	@PostMapping
 	public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO book, UriComponentsBuilder uriComponentsBuilder) {
 		Long id = bookService.createBook(book);
 
-		return ResponseEntity
-				.created(uriComponentsBuilder.path("/api/book/{id}").build(id))
-				.build();
+		return ResponseEntity.created(uriComponentsBuilder.path("/api/book/{id}").build(id)).build();
 
 	}
 }
