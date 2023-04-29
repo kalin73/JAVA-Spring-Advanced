7792fee02d005582620ea1d41e12dcdcbd264f83
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-04-30 02:43:46 +0300

    fix

--- Spring-security/src/main/java/com/softuni/security/config/SecurityConfiguration.java
+++ Spring-security/src/main/java/com/softuni/security/config/SecurityConfiguration.java
@@ -18,14 +18,25 @@ import com.softuni.security.web.ApplicationUserDetailsService;
 public class SecurityConfiguration {
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests().requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+		// defines which pages will be authorized
+		http.authorizeHttpRequests()
+				// allow access to all static files (images, CSS, js)
+				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+				// the URL-s below are available for all users - logged in and anonymous
 				.requestMatchers("/", "/users/login", "/users/register", "/users/login-error").permitAll()
+				// only for moderators
 				.requestMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
+				// only for admins
 				.requestMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name()).anyRequest().authenticated().and()
+				// configure login with HTML form
 				.formLogin(login -> login.loginPage("/users/login")
+						// the names of the user name, password input fields in the custom login form
 						.usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
 						.passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
-						.defaultSuccessUrl("/", true).failureForwardUrl("/users/login-error"));
+						// where do we go after login
+						.defaultSuccessUrl("/").failureForwardUrl("/users/login-error"))
+				// go to homepage after logout
+				.logout(logout -> logout.logoutUrl("/users/logout").logoutSuccessUrl("/").invalidateHttpSession(true));
 
 		return http.build();
 	}
