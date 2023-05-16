e37807dbdf05dd58fcbb85104f13224d7a1cddbf
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-05-16 16:51:53 +0300

    Spring scheduler

---
+++ Spring-scheduling/src/main/java/com/softuni/scheduling/SpringSchedulingApplication.java
@@ -0,0 +1,15 @@
+package com.softuni.scheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulingApplication.class, args);
+	}
+
+}
