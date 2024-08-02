66f73fbe5d2cb37c7e1eac53df217ad838884fe0
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-02 23:53:44 +0300

    adding jwt service

---
+++ Mobilele-Security/src/main/java/com/softuni/mobilelesec/services/JwtService.java
@@ -0,0 +1,46 @@
+package com.softuni.mobilelesec.services;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private final String secret;
+
+    @Value("${jwt.expiration}")
+    private final long expiration;
+
+    public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long expiration) {
+        this.secret = secret;
+        this.expiration = expiration;
+    }
+
+
+    public String generateToken(String userId, Map<String, Object> claims) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userId)
+                .issuedAt(now)
+                .notBefore(now)
+                .expiration(new Date(now.getTime() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
