f8682032f5f9cd0de3e25b388a0e1051967b437b
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-05 12:39:41 +0300

    adding open api configuration

--- mobilele-offers/src/main/java/org/example/mobileleoffers/config/OpenApiConfig.java
+++ mobilele-offers/src/main/java/org/example/mobileleoffers/config/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package org.example.mobileleoffers.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,12 @@ public class OpenApiConfig {
     @Bean
     OpenAPI customOpenAPI() {
         OpenAPI openAPI = new OpenAPI();
+        openAPI.components(new Components().addSecuritySchemes("bearer-key",
+                new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+
         openAPI.setInfo(new Info().description("This is the mobilele micro service.")
                 .title("Mobilele offers API")
                 .version("0.0.1")
