158e04dc3e419b97db1518eb52dcde8390397c20
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-05 13:56:14 +0300

    refactoring

--- Mobilele-Security/src/main/java/com/softuni/mobilelesec/services/UserService.java
+++ Mobilele-Security/src/main/java/com/softuni/mobilelesec/services/UserService.java
@@ -1,38 +1,53 @@
 package com.softuni.mobilelesec.services;
 
+import com.softuni.mobilelesec.domain.dtos.binding.UserRegisterFormDto;
+import com.softuni.mobilelesec.domain.entities.UserEntity;
+import com.softuni.mobilelesec.domain.user.MobileleUserDetails;
+import com.softuni.mobilelesec.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.softuni.mobilelesec.domain.dtos.binding.UserRegisterFormDto;
-import com.softuni.mobilelesec.domain.entities.UserEntity;
-import com.softuni.mobilelesec.repositories.UserRepository;
+import java.util.Optional;
 
 @Service
 public class UserService {
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
-	private final EmailService emailService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
-	public UserService(UserRepository userRepository, 
-			PasswordEncoder passwordEncoder, EmailService emailService) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.emailService = emailService;
-	}
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder, EmailService emailService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
+    }
 
-	public boolean isDbInit() {
-		return this.userRepository.count() > 0;
-	}
+    public boolean isDbInit() {
+        return this.userRepository.count() > 0;
+    }
 
-	public void registerUser(UserRegisterFormDto userRegister) {
-		UserEntity userToSave = new UserEntity().setFirstName(userRegister.getFirstName())
-				.setLastName(userRegister.getLastName()).setEmail(userRegister.getEmail())
-				.setPassword(passwordEncoder.encode(userRegister.getPassword()));
+    public Optional<MobileleUserDetails> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-		this.userRepository.save(userToSave);
+        if (authentication != null && authentication.getPrincipal() instanceof MobileleUserDetails mobileleUserDetails) {
+            return Optional.of(mobileleUserDetails);
+        }
 
-		this.emailService.sendRegistrationEmail(userToSave.getEmail(),
-				userToSave.getFirstName() + " " + userToSave.getLastName());
-	}
+        return Optional.empty();
+
+    }
+
+    public void registerUser(UserRegisterFormDto userRegister) {
+        UserEntity userToSave = new UserEntity().setFirstName(userRegister.getFirstName())
+                .setLastName(userRegister.getLastName()).setEmail(userRegister.getEmail())
+                .setPassword(passwordEncoder.encode(userRegister.getPassword()));
+
+        this.userRepository.save(userToSave);
+
+        this.emailService.sendRegistrationEmail(userToSave.getEmail(),
+                userToSave.getFirstName() + " " + userToSave.getLastName());
+    }
 
 }
--- Mobilele-Security/src/main/java/com/softuni/mobilelesec/web/OfferController.java
+++ Mobilele-Security/src/main/java/com/softuni/mobilelesec/web/OfferController.java
@@ -1,77 +1,72 @@
 package com.softuni.mobilelesec.web;
 
-import java.security.Principal;
-import java.util.UUID;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.softuni.mobilelesec.domain.dtos.binding.OfferCreationDto;
 import com.softuni.mobilelesec.domain.enums.Engine;
 import com.softuni.mobilelesec.domain.enums.ModelCategory;
 import com.softuni.mobilelesec.domain.enums.Transmission;
+import com.softuni.mobilelesec.domain.user.MobileleUserDetails;
 import com.softuni.mobilelesec.services.OfferService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/offers")
 public class OfferController {
-	private final OfferService offerService;
+    private final OfferService offerService;
 
-	public OfferController(OfferService offerService) {
-		this.offerService = offerService;
-	}
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
 
-	@PreAuthorize("@offerService.isOwner(#userDetails, #id)")
-	@DeleteMapping("/{id}")
-	public String deleteOffer(@AuthenticationPrincipal UserDetails userDetails, @PathVariable("id") UUID id) {
-		offerService.deleteOfferByUUID(id);
+    @PreAuthorize("@offerService.isOwner(#mobileleUserDetails, #id)")
+    @DeleteMapping("/{id}")
+    public String deleteOffer(@AuthenticationPrincipal MobileleUserDetails mobileleUserDetails, @PathVariable("id") UUID id) {
+        offerService.deleteOfferByUUID(id);
 
-		return "redirect:/offers/all";
-	}
+        return "redirect:/offers/all";
+    }
 
-	@GetMapping("/all")
-	public String getAllOffers(Model model, @PageableDefault(sort = "offerId", size = 3) Pageable pageable) {
-		var allOffersPage = offerService.getAllOffers(pageable);
+    @GetMapping("/all")
+    public String getAllOffers(Model model, @PageableDefault(sort = "offerId", size = 3) Pageable pageable) {
+        var allOffersPage = offerService.getAllOffers(pageable);
 
-		model.addAttribute("offers", allOffersPage);
+        model.addAttribute("offers", allOffersPage);
 
-		return "offers";
-	}
+        return "offers";
+    }
 
-	@GetMapping("/{id}")
-	public String getOfferById(@PathVariable(name = "id") UUID offerUUID, Model model,
-			@AuthenticationPrincipal UserDetails userDetails) {
-		model.addAttribute("offer", offerService.getOfferById(offerUUID));
-		model.addAttribute("canDelete", offerService.isOwner(userDetails, offerUUID));
+    @GetMapping("/{id}")
+    public String getOfferById(@PathVariable(name = "id") UUID offerUUID, Model model,
+                               @AuthenticationPrincipal MobileleUserDetails mobileleUserDetails) {
+        model.addAttribute("offer", offerService.getOfferById(offerUUID));
+        model.addAttribute("canDelete", offerService.isOwner(mobileleUserDetails, offerUUID));
 
-		return "details";
-	}
+        return "details";
+    }
 
-	@GetMapping("/add")
-	public ModelAndView getOfferAddPage(ModelAndView modelAndView) {
-		modelAndView.addObject("engines", Engine.values());
-		modelAndView.addObject("models", ModelCategory.values());
-		modelAndView.addObject("transmissions", Transmission.values());
-		modelAndView.setViewName("offer-add");
+    @GetMapping("/add")
+    public ModelAndView getOfferAddPage(ModelAndView modelAndView) {
+        modelAndView.addObject("engines", Engine.values());
+        modelAndView.addObject("models", ModelCategory.values());
+        modelAndView.addObject("transmissions", Transmission.values());
+        modelAndView.setViewName("offer-add");
 
-		return modelAndView;
-	}
+        return modelAndView;
+    }
 
-	@PostMapping("/add")
-	public String addOffer(OfferCreationDto creationDto, Principal principal) {
-		this.offerService.addOffer(creationDto, principal.getName());
+    @PostMapping("/add")
+    public String addOffer(OfferCreationDto creationDto, Principal principal) {
+        this.offerService.addOffer(creationDto, principal.getName());
 
-		return "redirect:/offers/all";
-	}
+        return "redirect:/offers/all";
+    }
 }
