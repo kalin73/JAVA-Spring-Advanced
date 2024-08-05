d539da292ba176e5317fe84d844550dcf78cb348
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-05 19:45:03 +0300

    jwt auth

---
+++ mobilele-offers/src/main/java/org/example/mobileleoffers/security/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package org.example.mobileleoffers.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.mobileleoffers.service.JwtService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null) {
+            final String jwtToken = authorizationHeader.replace("Bearer ", "");
+
+            UserDetails user = jwtService.extractUserDetails(jwtToken);
+
+            if (SecurityContextHolder.getContext().getAuthentication() == null) {
+                final Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
