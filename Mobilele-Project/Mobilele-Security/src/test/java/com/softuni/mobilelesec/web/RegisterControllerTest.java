b5682acec076d39eeafc27b1d21cc3eec505e760
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-08 19:03:02 +0300

    fixing tests

--- Mobilele-Project/Mobilele-Security/src/test/java/com/softuni/mobilelesec/web/RegisterControllerTest.java
+++ Mobilele-Project/Mobilele-Security/src/test/java/com/softuni/mobilelesec/web/RegisterControllerTest.java
@@ -1,12 +1,8 @@
 package com.softuni.mobilelesec.web;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,64 +13,66 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import com.icegreen.greenmail.util.GreenMail;
-import com.icegreen.greenmail.util.ServerSetup;
-
-import jakarta.mail.internet.MimeMessage;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RegisterControllerTest {
-	
-	@Autowired
-	private MockMvc mockMvc;
-	
-	@Value("${mail.host}")
-	private String mailHost;
-	
-	@Value("${mail.port}")
-	private Integer mailPort;
-	
-	@Value("${mail.username}")
-	private String username;
-	
-	@Value("${mail.password}")
-	private String password;
-	
-	
-	private GreenMail greenMail;
-	
-	@BeforeEach
-	void setUp() {
-		greenMail = new GreenMail(new ServerSetup(mailPort, mailHost, "smtp"));
-		greenMail.start();
-		greenMail.setUser(username, password);
-	}
-	
-	@AfterEach
-	void tearDown() {
-		greenMail.stop();
-	}
 
-	@Test
-	void testRegistration() throws Exception {
-	   // Act
-	   ResultActions result = mockMvc.perform(post("/users/register").
-	        param("email", "pesho@example.com").
-	        param("firstName", "Pesho").
-	        param("lastName", "Petrov").
-	        param("password", "topsecret").
-	        param("confirmPassword", "topsecret").
-	        with(csrf()));
-	   
-	    // Assert
-	    result.andExpect(status().is3xxRedirection())
-	    .andExpect(redirectedUrl("/users/login"));
-	    
-	    MimeMessage[] mails = greenMail.getReceivedMessages();
-	    assertEquals(1, mails.length);
-	    
-	    MimeMessage welcomeMessage = mails[0];
-	    assertTrue(welcomeMessage.getContent().toString().contains("Pesho Petrov"));
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Value("${spring.mail.host}")
+    private String mailHost;
+
+    @Value("${spring.mail.port}")
+    private Integer mailPort;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+
+    private GreenMail greenMail;
+
+    @BeforeEach
+    void setUp() {
+        greenMail = new GreenMail(new ServerSetup(mailPort, mailHost, "smtp"));
+        greenMail.start();
+        greenMail.setUser(username, password);
+    }
+
+    @AfterEach
+    void tearDown() {
+        greenMail.stop();
+    }
+
+    @Test
+    void testRegistration() throws Exception {
+        // Act
+        ResultActions result = mockMvc.perform(post("/users/register").
+                param("email", "pesho@example.com").
+                param("firstName", "Pesho").
+                param("lastName", "Petrov").
+                param("password", "topsecret").
+                param("confirmPassword", "topsecret").
+                with(csrf()));
+
+        // Assert
+        result.andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/login"));
+
+        MimeMessage[] mails = greenMail.getReceivedMessages();
+        assertEquals(1, mails.length);
+
+        MimeMessage welcomeMessage = mails[0];
+        assertTrue(welcomeMessage.getContent().toString().contains("Pesho Petrov"));
+    }
 }
