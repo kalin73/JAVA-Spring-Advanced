f8844e2e40ca06358e7e3dc01757269fafb4a1fe
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-13 17:31:38 +0300

    creating model mapper bean

---
+++ Mobilele-Project/mobilele-offers/src/main/java/org/example/mobileleoffers/config/BeanConfig.java
@@ -0,0 +1,13 @@
+package org.example.mobileleoffers.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
